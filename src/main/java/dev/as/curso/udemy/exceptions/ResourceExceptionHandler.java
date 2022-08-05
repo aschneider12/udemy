e982519ed67e3851b62c30b0e0114c3c204e3699
ASchneider <schn.alessandro@gmail.com>
2022-08-05 11:51:02 -0400

    resource handler

--- src/main/java/dev/as/curso/udemy/exceptions/ResourceExceptionHandler.java
+++ src/main/java/dev/as/curso/udemy/exceptions/ResourceExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@ControllerAdvice //interceptor
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
